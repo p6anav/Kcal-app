13d9cdcbf173f5f38bc5feea100ac30aa385708a
P6ANAV <100874811+p6anav@users.noreply.github.com>
2024-04-26 00:33:00 +0530

    Update main.dart

--- Login/main.dart
+++ Login/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'signup_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login and Sign Up',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/signup': (context) => SignUpPage(),
+      },
+    );
+  }
+}
 
