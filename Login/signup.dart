7ad210e043e8104e880def95ecad762d96fb2ed3
P6ANAV <100874811+p6anav@users.noreply.github.com>
2024-04-27 12:48:36 +0530

    Update signup.dart

--- Login/signup.dart
+++ Login/signup.dart
@@ -1,1 +1,221 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignupPage(),
+    );
+  }
+}
+
+class SignupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Sign Up')),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(bottom: 20.0),
+        child: Padding(
+          padding: EdgeInsets.all(screenWidth * 0.05),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: screenHeight * 0.04,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Create Account',
+                      style: TextStyle(
+                        fontFamily: 'Viga',
+                        fontSize: screenWidth * 0.08,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.02,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Please fill to create an account',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.03,
+                        fontFamily: 'Viga',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.04,
+                  ),
+                  TextFormField(
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenWidth * 0.04,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 240, 240, 240),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.025,
+                        horizontal: screenWidth * 0.04,
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.02,
+                  ),
+                  TextFormField(
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenWidth * 0.04,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 240, 240, 240),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.025,
+                        horizontal: screenWidth * 0.04,
+                      ),
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.02,
+                  ),
+                  TextFormField(
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenWidth * 0.04,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 240, 240, 240),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.025,
+                        horizontal: screenWidth * 0.04,
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.02,
+                  ),
+                  TextFormField(
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: screenWidth * 0.04,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 240, 240, 240),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.025,
+                        horizontal: screenWidth * 0.04,
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.04,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: MaterialButton(
+                      onPressed: () {
+                        // Action for the "Sign Up" button
+                      },
+                      color: Colors.black,
+                      textColor: Colors.white,
+                      padding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.025,
+                        horizontal: screenWidth * 0.04,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(screenWidth * 0.05),
+                      ),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(fontSize: screenWidth * 0.04),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.02,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Already have an account? Login",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Image.asset(
+                  'assets/images/pix.png',
+                  height: screenHeight * 0.25,
+                  width: screenWidth * 0.25,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
